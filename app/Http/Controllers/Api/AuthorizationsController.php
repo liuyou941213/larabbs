90abdb93aa257dd81b935cef6375d5c76028ed6e
vagrant <vagrant@homestead>
2020-07-30 18:15:38 +0000

    Passport 第三方登录

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -13,9 +13,13 @@ use Psr\Http\Message\ServerRequestInterface;
 use League\OAuth2\Server\AuthorizationServer;
 use Zend\Diactoros\Response as Psr7Response;
 use League\OAuth2\Server\Exception\OAuthServerException;
+use App\Traits\PassportToken;
+
 
 class AuthorizationsController extends Controller
 {
+    use PassportToken;
+
     public function store(AuthorizationRequest $originRequest, AuthorizationServer $server, ServerRequestInterface $serverRequest)
     {
         try {
@@ -69,9 +73,9 @@ class AuthorizationsController extends Controller
             break;
         }
 
-        $token= auth('api')->login($user);
+        $result = $this->getBearerTokenByUser($user, '1', false);
 
-        return $this->respondWithToken($token)->setStatusCode(201);
+        return response()->json($result)->setStatusCode(201);
     }
 
     protected function respondWithToken($token)
